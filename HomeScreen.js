ca4237216852e8a7fad877808c672f74a5044d9b
WilsonLee2000 <leejunwei2000@gmail.com>
2022-05-25 16:24:22 +0800

    Added the bottom navigation

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,14 +1,81 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const HomeScreen = () => {
+function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> Welcome to DestiNUS </Text>
     </View>
   );
-};
+}
 
-export default HomeScreen;
+function NotificationsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications will be displayed here!</Text>
+    </View>
+  );
+}
+
+function LocationsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> Locations will be displayed here! </Text>
+    </View>
+  );
+}
+
+function BookingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text> Bookings are made here! </Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Notifications') {
+              iconName = focused 
+               ? 'ios-notifications' 
+               : 'ios-notifications-outline';
+            } else if (route.name === 'Locations') {
+              iconName = focused
+               ? 'ios-navigate'
+               : 'ios-navigate-outline';
+            } else if (route.name === 'Bookings') {
+              iconName = focused
+              ? 'ios-calendar'
+              : 'ios-calendar-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'black',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        <Tab.Screen name="Locations" component={LocationsScreen} />
+        <Tab.Screen name="Bookings" component={BookingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
