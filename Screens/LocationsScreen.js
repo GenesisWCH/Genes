07946732f9ade1db1e3afe5b43aca47f1a131cf3
Tan Cheong Hsien <cheonghsien@gmail.com>
2022-06-06 21:33:59 +0800

    Add blank map to location tab and remove tab header for the tab

--- Screens/LocationsScreen.js
+++ Screens/LocationsScreen.js
@@ -1,12 +1,73 @@
 import React, { useState } from "react";
 import { Text, View } from 'react-native';
-import styles from '../css/TemplateScreenStyle';
+import { AntDesign } from '@expo/vector-icons';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import Modal from "react-native-modal";
+import { SafeAreaView } from "react-native-safe-area-context";
+import LogOutHandler from "../functions/LogOutHandler";
+import { auth } from '../firebase';
+
+import styles from "../css/LocationsStyle";
+import MapView from 'react-native-maps';
 
 function LocationsScreen() {
-    return (
-      <View style={styles.page}>
-        <Text> Locations will be displayed here! </Text>
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <SafeAreaView style={styles.page}>
+      <Modal
+        animationIn={"slideInRight"}
+        animationOut={'slideOutRight'}
+        isVisible={modalVisible}
+        onBackdropPress={() => setModalVisible(false)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <AntDesign style={styles.modalProfileIcon} name="user" size={28} color='black' />
+            <Pressable
+              style={[styles.button]}
+            //</View>onPress={}
+            >
+              <Text style={styles.textStyle}>Profile</Text>
+
+            </Pressable>
+            <Pressable
+              style={[styles.button]}
+            //onPress={}
+            >
+              <Text style={styles.textStyle}>Settings</Text>
+
+            </Pressable>
+            <Pressable
+              style={[styles.button]}
+            //</View>onPress={}
+            >
+              <Text style={styles.textStyle}>About</Text>
+
+            </Pressable>
+            <Pressable
+              style={[styles.button]}
+              onPress={LogOutHandler}
+            >
+              <Text style={styles.textStyle}>Sign Out</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <View style={styles.header}>
+        <Text style={styles.headerText}> Home</Text>
+        <Pressable
+          style={styles.profileIcon}
+          onPress={() => setModalVisible(true)}
+        >
+          <AntDesign name="user" size={28} color='black' />
+        </Pressable>
+
       </View>
+      <View style={styles.body}>
+        <MapView style={styles.map} />
+      </View>
+      </SafeAreaView>
     );
   }
 
