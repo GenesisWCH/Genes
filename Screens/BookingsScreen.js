694065118112a03a52543611edda3933ee40113d
Tan Cheong Hsien <cheonghsien@gmail.com>
2022-06-22 00:04:47 +0800

    Update booking

--- Screens/BookingsScreen.js
+++ Screens/BookingsScreen.js
@@ -7,8 +7,9 @@ import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 import Modal from "react-native-modal";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LogOutHandler from "../functions/LogOutHandler";
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 import { Dropdown } from 'react-native-element-dropdown';
+import { collection, collectionGroup, query, where, getDocs, Firestore } from "firebase/firestore";
 
 const levelData = [
   { label: 'Level 1', level: '1' },
@@ -16,8 +17,8 @@ const levelData = [
 ];
 
 const roomTypeData = [
-  { label: 'Seminar', room: 'Seminar' },
-  { label: 'Tutorial', room: 'Tutorial' },
+  { label: 'Seminar', room: 'seminar' },
+  { label: 'Tutorial', room: 'tutorial' },
 ]
 
 const dateData = [
@@ -36,17 +37,88 @@ function BookingsScreen() {
   const [roomType, setRoomType] = useState(null);
   const [date, setDate] = useState(null);
   const [isFocus, setIsFocus] = useState(false);
+  const [data, setData] = useState(null);
+  const [dummyData, setDummyData] = useState(null);
 
-  // const renderLabel = () => {
-  //   if (level || isFocus) {
-  //     return (
-  //       <Text style={[styles.label, isFocus && { color: 'blue' }]}>
-  //         Dropdown label
-  //       </Text>
-  //     );
-  //   }
-  //   return null;
-  // };
+  const currDate = new Date()
+
+  const getMonthWord = (month) => {
+    // switch cases for converting 0 to 11 to strings by month
+    switch (month) {
+      case 0:
+        return "Jan"
+      case 1:
+        return "Feb"
+      case 2:
+        return "Mar"
+      case 3:
+        return "Apr"
+      case 4:
+        return "May"
+      case 5:
+        return "Jun"
+      case 6:
+        return "Jul"
+      case 7:
+        return "Aug"
+      case 8:
+        return "Sep"
+      case 9:
+        return "Oct"
+      case 10:
+        return "Nov"
+      case 11:
+        return "Dec"
+      default:
+        return;
+    }
+  }
+
+  const filterByLevelType = async (level, roomType) => {
+    setData([])
+    setDummyData([])
+    const futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + 14)
+    const datesAvail = query(collectionGroup(db, 'bookings'), where('type', '==', roomType),
+    where('level', '==', level));
+      const querySnapshot = await getDocs(datesAvail);
+      querySnapshot.forEach((doc) => {
+        // if (data.some(e => e.date == doc.date)) { // if date is already in array collection
+
+        // }
+        if (!dummyData.includes(doc.date)) {
+          console.log(doc.date)
+          // dummyData.push(doc.date)
+          // const day = doc.date.toDate().getDate().toString()
+          // const month = getMonthWord(doc.date.toDate().getMonth())
+          // const year = doc.date.toDate().getFullYear().toString()
+          // const str = day + " " + month + " " + year
+          // console.log(str)
+          // data.push({ label: str, date: doc.date })
+        }
+      });
+    }
+  
+    // ,
+    // where('level', '==', level), where('valid', '==', true), where('status', '==', 'available')
+
+  // const filterTest = async (level, roomType) => {
+  //   // setData([])
+  //   // const futureDate = new Date();
+  //   // futureDate.setDate(futureDate.getDate() + 14)
+  //   const datesAvail = query(collection(db, 'rooms'), where('type', '==', roomType));
+  //   const querySnapshot = await getDocs(datesAvail);
+  //   querySnapshot.forEach((doc) => {
+  //     console.log(doc.id, ' => ', doc.data());
+  //   });
+
+  //   querySnapshot.forEach((doc) => {
+  //     console.log(doc.id, ' => ', doc.data());
+  //   });
+  // }
+
+
+
 
   return (
     <SafeAreaView style={styles.page}>
@@ -100,8 +172,8 @@ function BookingsScreen() {
         {auth.currentUser.isAnonymous ?
           <Text styles={styles.headerText}> You are a guest. Bookings are only available to NUS staff and students.</Text>
           : <Text styles={styles.headerText}>Bookings are made here!</Text>}
-        {/* {renderLabel()} */}
         <Dropdown
+          // level
           style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
           placeholderStyle={styles.placeholderStyle}
           selectedTextStyle={styles.selectedTextStyle}
@@ -131,6 +203,7 @@ function BookingsScreen() {
           )}
         />
         <Dropdown
+          // type
           style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
           placeholderStyle={styles.placeholderStyle}
           selectedTextStyle={styles.selectedTextStyle}
@@ -149,6 +222,9 @@ function BookingsScreen() {
           onChange={item => {
             setRoomType(item.room);
             setIsFocus(false);
+            // filterTest(level, roomType)
+            filterByLevelType(level, roomType)
+            console.log('yes there is change!')
           }}
           renderLeftIcon={() => (
             <AntDesign
@@ -160,6 +236,7 @@ function BookingsScreen() {
           )}
         />
         <Dropdown
+          //date
           style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
           placeholderStyle={styles.placeholderStyle}
           selectedTextStyle={styles.selectedTextStyle}
@@ -188,71 +265,9 @@ function BookingsScreen() {
             />
           )}
         />
-        <Text style={styles.timeText}>Time:</Text>
-        <View style={styles.timeContainer}>
-          <Dropdown
-            style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
-            placeholderStyle={styles.placeholderStyle}
-            selectedTextStyle={styles.selectedTextStyle}
-            inputSearchStyle={styles.inputSearchStyle}
-            iconStyle={styles.iconStyle}
-            data={dateData}
-            search
-            maxHeight={300}
-            labelField="label"
-            valueField="date"
-            placeholder='Select Date'
-            searchPlaceholder="Search..."
-            value={date}
-            onFocus={() => setIsFocus(true)}
-            onBlur={() => setIsFocus(false)}
-            onChange={item => {
-              setDate(item.date);
-              setIsFocus(false);
-            }}
-            renderLeftIcon={() => (
-              <AntDesign
-                style={styles.icon}
-                color={isFocus ? 'blue' : 'black'}
-                name="Safety"
-                size={20}
-              />
-            )}
-          />
-          <Text style={styles.timeText}>to:</Text>
-          <Dropdown
-            style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
-            placeholderStyle={styles.placeholderStyle}
-            selectedTextStyle={styles.selectedTextStyle}
-            inputSearchStyle={styles.inputSearchStyle}
-            iconStyle={styles.iconStyle}
-            data={dateData}
-            search
-            maxHeight={300}
-            labelField="label"
-            valueField="date"
-            placeholder='Select Date'
-            searchPlaceholder="Search..."
-            value={date}
-            onFocus={() => setIsFocus(true)}
-            onBlur={() => setIsFocus(false)}
-            onChange={item => {
-              setDate(item.date);
-              setIsFocus(false);
-            }}
-            renderLeftIcon={() => (
-              <AntDesign
-                style={styles.icon}
-                color={isFocus ? 'blue' : 'black'}
-                name="Safety"
-                size={20}
-              />
-            )}
-          />
-
-        </View>
+        
         <Pressable style={styles.searchButton}>
-<Text style={styles.searchButtonText}>Search</Text>
+          <Text style={styles.searchButtonText}>Search</Text>
         </Pressable>
       </View>
 
