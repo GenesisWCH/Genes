8979113b881c1652f378b448ddb7bfbe5695153a
Tan Cheong Hsien <cheonghsien@gmail.com>
2022-06-26 23:44:54 +0800

    Code cleanup

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,6 +1,6 @@
 // Code is a modified version of RN workshop given codes by Dominic and Marcus
 import {
-    StyleSheet, Text, View, Image, Pressable, TextInput, Dimensions,
+    Text, View, Image, Pressable, TextInput,
     Keyboard, KeyboardAvoidingView
 } from "react-native";
 import React, { useState } from 'react';
@@ -8,8 +8,7 @@ import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } 
 import { auth } from '../firebase/index';
 import Modal from "react-native-modal";
 import Toast from 'react-native-root-toast';
-
-const { width } = Dimensions.get('window');
+import styles from '../css/SignUpStyle'
 
 const SignUpPage = () => {
     const [name, setName] = useState('');
@@ -233,91 +232,3 @@ const SignUpPage = () => {
 };
 
 export default SignUpPage;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    image: {
-        width: width * 0.7,
-        height: 240,
-        alignSelf: 'center'
-    },
-    inputContainer: {
-        alignItems: 'center'
-    },
-    input: {
-        marginBottom: 10,
-        width: 250,
-        height: 30,
-        paddingHorizontal: 8,
-        backgroundColor: '#D9D9D9',
-
-    },
-    input2: {
-        marginVertical: 10,
-        width: 250,
-        height: 30,
-        paddingHorizontal: 8,
-        backgroundColor: '#D9D9D9',
-
-    },
-    button: {
-        backgroundColor: '#75C3FB',
-        marginTop: 46,
-        paddingHorizontal: 5,
-        paddingVertical: 8,
-        borderRadius: 5,
-    },
-    buttonText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: 'black',
-    },
-    responseText: {
-        color: 'black',
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 40,
-        backgroundColor: '#D9D9D9'
-    },
-    centeredView: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    modalView: {
-        backgroundColor: "white",
-        padding: 15,
-        alignItems: "center",
-    },
-    modalText: {
-        textAlign: 'left'
-    },
-    modalButton: {
-        backgroundColor: '#75C3FB',
-        paddingHorizontal: 5,
-        paddingVertical: 8,
-        borderRadius: 5,
-    },
-    modalButtonText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: 'black',
-    },
-    passwordRequirementButton: {
-        width: 250,
-        height: 30,
-        paddingHorizontal: 8,
-        backgroundColor: '#FFFFFF',
-    },
-    passwordRequirementButtonText: {
-        fontSize: 12,
-        fontWeight: 'bold',
-        textDecorationLine: 'underline',
-        color: '#0B5497'
-    },
-});
--- css/SignUpStyle.js
+++ css/SignUpStyle.js
@@ -1,4 +1,6 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
 
 const SignUpStyle = StyleSheet.create({
     container: {
@@ -8,7 +10,6 @@ const SignUpStyle = StyleSheet.create({
         alignItems: 'center'
     },
     image: {
-        marginTop: 0,
         width: width * 0.7,
         height: 240,
         alignSelf: 'center'
@@ -17,12 +18,12 @@ const SignUpStyle = StyleSheet.create({
         alignItems: 'center'
     },
     input: {
-        marginTop: 0,
         marginBottom: 10,
         width: 250,
         height: 30,
         paddingHorizontal: 8,
         backgroundColor: '#D9D9D9',
+
     },
     input2: {
         marginVertical: 10,
@@ -30,6 +31,7 @@ const SignUpStyle = StyleSheet.create({
         height: 30,
         paddingHorizontal: 8,
         backgroundColor: '#D9D9D9',
+
     },
     button: {
         backgroundColor: '#75C3FB',
@@ -49,7 +51,43 @@ const SignUpStyle = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 40,
         backgroundColor: '#D9D9D9'
-    }
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalView: {
+        backgroundColor: "white",
+        padding: 15,
+        alignItems: "center",
+    },
+    modalText: {
+        textAlign: 'left'
+    },
+    modalButton: {
+        backgroundColor: '#75C3FB',
+        paddingHorizontal: 5,
+        paddingVertical: 8,
+        borderRadius: 5,
+    },
+    modalButtonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    passwordRequirementButton: {
+        width: 250,
+        height: 30,
+        paddingHorizontal: 8,
+        backgroundColor: '#FFFFFF',
+    },
+    passwordRequirementButtonText: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+        color: '#0B5497'
+    },
 });
 
 export default SignUpStyle;
